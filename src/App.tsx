7f2467876800341e7f1b6a88681d936a070bbbf1
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2024-05-11 22:37:31 -0300

    feat: imported the ContextProvider into the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import Header from "./components/Header/Header.component"
 import './assets/style/index.css'
 import Hero from "./components/Hero/Hero.component"
+import { CartContextProvider } from "./components/Context/CartContext"
 
 function App() {
 
   return (
-    <>
+    <CartContextProvider>
       <Header/>
       <Hero/>
-    </>
+    </CartContextProvider>
   )
 }
 
